8c44e0c23a221957c859e325c44394c85a953c52
mopig <mopig@foxmail.com>
2014-03-25 16:03:52 +0800

    to test 'greet'

---
+++ test/greet_spec.js
@@ -0,0 +1,10 @@
+var expect = require("chai").expect;
+var greet = require("../index.js");
+
+describe('greet',function () {
+    it("should greet a person by name",function(){
+        expect(greet('jj',false)).to.eql('hello, jj');
+    });
+    it("should greet a person flirtatiously if drunk",function(){
+        expect(greet('jj', true)).to.eql('hello jj, you look sexy today')});
+});
