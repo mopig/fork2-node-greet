bcbdd44e57fbcef0f5abcaba5439fcd35fdcd99d
mopig <mopig@foxmail.com>
2014-03-26 16:03:36 +0800

    rm waste

--- test/greet_spec.js
+++
@@ -1,9 +0,0 @@
-var greet = require("../lib/index.js");
-
-describe('greet',function () {
-    it("should greet a person by name",function(){
-        expect(greet('jj',false)).to.eql('hello, jj');
-    });
-    it("should greet a person flirtatiously if drunk",function(){
-        expect(greet('jj', true)).to.eql('hello jj, you look sexy today')});
-});
